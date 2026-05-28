f1f3827b4e0870f8d6f870af679d887ed39efa3a
Creighton Gorai <creighton.gorai@gmail.com>
2026-05-27 15:30:20 -1000

    Changing input logic to check for button clicks to evaluate the user input.

--- main.js
+++ main.js
@@ -30,18 +30,35 @@ const operate = function(operator, numOne, numTwo) {
 // Getting button clicks
 const buttons = document.querySelectorAll('button');
 
+// Each time the button is pressed check what was pressed and evaluate
 buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
         const screen = document.getElementById("screen");
-        const operatorRegex = /[+\-\x/%\.]/g;
-        if (screen.innerHTML == 0) {
-            if (!operatorRegex.test(btn.textContent)) {
-                screen.innerHTML = btn.textContent;
-            }
-        } else if (btn.textContent == "AC") {
-            screen.innerHTML = 0;
+        const operatorRegex = /[+\-x/=]/g; // Currently only checking for plus, minus, multiply, divide
+
+        // If AC was clicked clear the screen
+        if (btn.textContent == "AC") {
+            screen.innerHTML = "0";
+
+        // Check button input
         } else {
-            screen.innerHTML += btn.textContent;
+            // If button was an operator
+            if (operatorRegex.test(btn.textContent)) {
+                // If there was no previous operator input. If true do nothing
+                if (!operatorRegex.test(screen.innerHTML[-1])) {
+                    console.log("We did it!");
+                }
+                
+            } else {
+                // If button was a number
+                // Replace zero with the number
+                if (screen.innerHTML == "0") {
+                    screen.innerHTML = btn.textContent;
+                // Else add it to the previous numbers
+                } else {
+                    screen.innerHTML += btn.textContent;
+                }
+            }
         }
     });
 });
